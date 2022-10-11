71340f16398b815b2959016354a31acadc0549f6
Manish <111450082+ManishModak@users.noreply.github.com>
2022-10-11 23:02:53 +0530

    Create EmployeeDatabase.java

---
+++ Java/Project No.5(Inheritance)/EmployeeDatabase.java
@@ -0,0 +1,78 @@
+import java.util.* ;
+public class EmployeeDatabase 
+{
+
+	public static void main(String[] args) 
+  {
+		
+		Scanner Sc = new Scanner(System.in) ;
+		DerivedClass E[] = new DerivedClass[20] ;
+		int choice , i = 0 , Age , Sr ;
+		String Name , Address ; 
+		
+		do
+		{
+			System.out.println("\nEnter 1 To Insert") ;
+			System.out.println("Enter 2 To Search") ;
+			System.out.println("Enter 3 To Replace") ;
+			System.out.println("Enter 4 To Display") ;
+			System.out.print("Enter The Choice : ");
+			choice = Sc.nextInt() ;
+			
+			switch (choice)
+			{
+				case 1 :
+					
+					System.out.print("\nEnter Name Of Employee : ") ;
+					Name = Sc.next() ;
+					System.out.print("\nEnter Age Of Employee : ") ;
+					Age = Sc.nextInt() ;
+					System.out.print("\nEnter Address Of Employee : ") ;
+					Address = Sc.next();
+					
+					E[i] = new DerivedClass(Name , Age , Address) ;
+					i ++ ;
+					break ;
+					
+				case 2 :
+					System.out.print("Enter the Sr no. Want to Search : ") ;
+					Sr = Sc.nextInt();
+					System.out.println("\n"+"\t"+ "Name" +"\t"+ "Age" +"\t"+ "Address") ;
+					E[Sr-1].Display();
+					break ;
+					
+				case 3 :
+					
+					System.out.print("Enter the Sr no. Want to Override : ") ;
+					Sr = Sc.nextInt();
+					System.out.print("\nEnter Name Of Employee : ") ;
+					Name = Sc.next() ;
+					System.out.print("\nEnter Age Of Employee : ") ;
+					Age = Sc.nextInt() ;
+					System.out.print("\nEnter Address Of Employee : ") ;
+					Address = Sc.next();
+					
+					E[Sr-1] = new DerivedClass(Name , Age , Address) ; 
+					//E[Sr-1] = E[i] ;
+					break ;
+					
+				case 4 :
+					
+					System.out.println("\n"+"Sr" +"\t"+ "Name" +"\t"+ "Age" +"\t"+ "Address") ;
+					for (int j = 0 ; j < i ; j++)
+					{
+						System.out.print(j+1);
+						E[j].Display();
+					}
+					break ;
+					
+				case 5 :
+					
+					System.out.println("Operation Has Ended ") ;
+			}
+      
+		}while(choice != 5) ;
+
+	}
+
+}
