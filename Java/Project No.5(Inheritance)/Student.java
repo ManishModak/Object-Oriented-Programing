089f2542a79830af58782382e8b1fa3ffb86c43c
Manish <111450082+ManishModak@users.noreply.github.com>
2022-10-11 23:05:07 +0530

    Update Student.java

--- Java/Project No.5(Inheritance)/Student.java
+++ Java/Project No.5(Inheritance)/Student.java
@@ -3,7 +3,7 @@ Public class Student
 	private String name ;
 	private int age ;
 	
-	public BaseClass (String name , int age)
+	public Student (String name , int age)
 	{
 		this.name = name ;
 		this.age = age ;
