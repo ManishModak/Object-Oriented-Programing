6953b4576387ef9e2c6ee939ff283746ba390770
Manish <111450082+ManishModak@users.noreply.github.com>
2022-11-16 19:05:21 +0530

    Create Rectangle.java

---
+++ Java/Project No.6(Hierarchical Inheritance)/Rectangle.java
@@ -0,0 +1,24 @@
+import java.util.* ;
+
+public class Rectangle extends Shape                                      // Base class inherited
+{
+	Scanner Sc = new Scanner(System.in) ;
+	private double area ;
+	
+	public void getdata()                                                  // Declaring methods according to our needs
+	{
+		System.out.print("Enter Length : ") ;
+		x = Sc.nextDouble() ;
+		
+		System.out.print("Enter Breadth : ") ;
+		y = Sc.nextDouble() ;
+	}
+	
+	public void compute_area()
+	{
+		area = x * y ;
+		System.out.println("Area of Rectangle is " + area + "\n") ;
+		
+	}
+	
+}
